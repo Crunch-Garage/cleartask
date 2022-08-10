b21a05fc8379a57feeb301a8f7a8fed2fbff968f
reiikennedy <mutuakennedy81@gmail.com>
2022-08-10 22:10:19 +0300

    feat:create accounts app

--- cleartask/urls.py
+++ cleartask/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/', include('project.urls'))
+    path('projects/', include('project.urls')),
+    path('accounts/',include('accounts.urls'))
     
 ]
 
