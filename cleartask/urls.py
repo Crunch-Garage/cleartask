b7e32f1cb920bb9289024b625a26578034108518
reiikennedy <mutuakennedy81@gmail.com>
2022-08-10 21:46:22 +0300

    feat: add static file urls to main urls file

--- cleartask/urls.py
+++ cleartask/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('project.urls'))
     
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
