c33bc383db9e1f9fbd472ee57cc84d1e03edba24
reiikennedy <mutuakennedy81@gmail.com>
2022-08-13 19:55:08 +0300

    feat:add phone field to user model. We will use this field to allow users to signup with phone numbers

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,17 @@
+from distutils.command.upload import upload
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # extending user model to make it easy later to add custom fields to user table
 class User(AbstractUser):
-    pass
+    # Make sure the phone number is in the correct format
+    phone_regex = RegexValidator( regex   =r'^\+?1?\d{9,14}$', message ="Phone number must be entered in the format: '+254xxxxxxxxx'. Up to 14 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, unique=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Users"
+    
+    def __str__(self):
+        return f"{self.username} - {self.email}"
