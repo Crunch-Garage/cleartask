d4033facdfe0ce5e7b31bd4835cb43ade605b1c8
reiikennedy <mutuakennedy81@gmail.com>
2022-08-13 18:41:29 +0300

    feat:register task model to admin dashboard

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class TaskPhotosInline(admin.StackedInline):
+	model = models.TaskPhotos
+	extra = 1
+
+class TaskAdmin(admin.ModelAdmin):
+    inlines = [TaskPhotosInline]
+    list_display = ("title","created_by","assigned_to")
+
+admin.site.register(models.Task,TaskAdmin)
