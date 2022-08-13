272cde4537aae87a47ab55ad5e56eb7a19bc74ef
reiikennedy <mutuakennedy81@gmail.com>
2022-08-13 19:59:19 +0300

    feat:add user model to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        *UserAdmin.fieldsets,  # original form fieldsets, expanded
+        (                      # new fieldset added on to the bottom
+            'Other Details',  # group heading of choice; set to None for a blank space instead of a header
+            {
+                'fields': (
+                    'phone_number',
+                ),
+            },
+        ),
+    )
+
+admin.site.register(models.User, CustomUserAdmin)
