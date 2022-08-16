09f98f4d95a0c5b7fff2f6d1c3e2d6926fc66c3f
reiikennedy <mutuakennedy81@gmail.com>
2022-08-16 23:13:47 +0300

    feat:add userlist api urls

--- accounts/apis/urls.py
+++ accounts/apis/urls.py
@@ -8,4 +8,5 @@ from . import views
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('users/list/',views.UserListApiView.as_view(), name="user_list")
 ]
