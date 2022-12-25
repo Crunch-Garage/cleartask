728bc0c57f6909fd5bff9d9e8fc20ca3aa6f753e
reiikennedy <mutuakennedy81@gmail.com>
2022-12-25 15:52:54 +0300

    fix: add conditional default settings file

--- manage.py
+++ manage.py
@@ -2,11 +2,16 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from cleartask.settings import base
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cleartask.settings')
+    if base.DEBUG==False:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cleartask.settings.prod')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cleartask.settings.local')
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cleartask.settings.prod')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
