a0ba852f0fdc0e4b9ee9f800070cccd83fc38bda
reiikennedy <mutuakennedy81@gmail.com>
2023-02-10 01:40:01 +0300

    feat: add new page links to footer

--- react/src/components/Footer.js
+++ react/src/components/Footer.js
@@ -1,9 +1,39 @@
 import "./Footer.css";
+import logo from '../assets/solid.png';
+import googlePlay from '../assets/google-play-badge.png';
 import {Container, Grid} from '@mui/material';
 
 const Footer = ()=>{
     return(
         <footer className="footer">
+            <Container className="footer__container">
+                    <Grid container xs="auto" spacing={5} className="footer__grid footer__grid--top">
+                        <Grid item xs="auto" className="footer__grid-item">
+                            <a href="/"><img src={logo} alt="Cleartask logo"/></a>
+                        </Grid>
+                        <Grid item xs="auto" className="footer__grid-item">
+                            <a href="#">
+                                <h3>About Cleartask</h3>
+                                <p>Why we created Cleartask.</p>
+                            </a>
+                        </Grid>
+                        <Grid item xs="auto" className="footer__grid-item">
+                            <a href="#">
+                                <h3>Careers</h3>
+                                <p>Check out available roles.</p>
+                            </a>
+                        </Grid>
+                        <Grid item xs="auto" className="footer__grid-item">
+                            <a href="#">
+                                <h3>Contact us</h3>
+                                <p>Need support? Talk to us.</p>
+                            </a>
+                        </Grid>
+                        <Grid item xs="auto" className="footer__grid-item app-badge">
+                            <a href="#"><img src={googlePlay} alt="Google play"/></a>
+                        </Grid>
+                    </Grid>
+            </Container>
             <Container className="footer__container">
                     <Grid container xs="auto" spacing={1} className="footer__grid">
                         <Grid item xs="auto" className="footer__grid-item">
