ccb210f41bbc0e665043952f20c9474d1822f2f1
reiikennedy <mutuakennedy81@gmail.com>
2023-02-14 11:20:13 +0300

    feat: add sendmail helper function

--- api/accounts/utility.py
+++ api/accounts/utility.py
@@ -1,4 +1,8 @@
 import re
+from django.contrib.sites.models import Site
+from django.core.mail import send_mail
+
+site_name = Site.objects.get_current().name
 
 # check valid email
 def email_is_valid(email):
@@ -6,4 +10,19 @@ def email_is_valid(email):
     if re.fullmatch(email_regex, email):
         return True
     else:
+        return False
+
+def send_email(email):
+    subject, message = "Verify your email", f"Get started on {site_name} by verifying your email"
+    try:
+        send_mail(
+            subject,
+            message,
+            '',
+            [email],
+            fail_silently=False,
+        )
+        return True
+    except Exception as e:
+        print(e)
         return False
