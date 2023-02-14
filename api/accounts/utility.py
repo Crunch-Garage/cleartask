37e702460beeeb2e2721e376965d8dc83aede68b
reiikennedy <mutuakennedy81@gmail.com>
2023-02-14 07:29:18 +0300

    feat: add email valid check helper function

---
+++ api/accounts/utility.py
@@ -0,0 +1,9 @@
+import re
+
+# check valid email
+def email_is_valid(email):
+    email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    if re.fullmatch(email_regex, email):
+        return True
+    else:
+        return False
