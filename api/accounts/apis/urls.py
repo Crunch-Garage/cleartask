2940553da8c94ebddcf841c6684c60095d090a96
reiikennedy <mutuakennedy81@gmail.com>
2023-02-14 18:49:29 +0300

    feat: add activate account api endpoint

--- api/accounts/apis/urls.py
+++ api/accounts/apis/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
+from django.conf.urls import url
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 from . import views
 
+app_name = 'accounts-apis'
+
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('users/list/',views.UserListApiView.as_view(), name="user_list"),
-    path('create_account/claim_email/',views.get_started_with_email, name="start_with_email")
+    path('create_account/claim_email/',views.get_started_with_email, name="start_with_email"),
+    url(r'^activate/(?P<uid>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate_account, name='activate_account'),
 ]
