5aa00e95a63452a341d3c036784a9cca4d0e61d0
reiikennedy <mutuakennedy81@gmail.com>
2023-02-18 22:34:15 +0300

    feat: create  global import for images.

---
+++ react/src/global/Images.js
@@ -0,0 +1,10 @@
+// All imports for images used in the project
+// This will make it easier to manage the across the project 
+
+import calendarApp from "../assets/calendar-app.jpg";
+
+const Images = {
+    calendarApp,
+}
+
+export default Images;
