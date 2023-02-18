335c365b29106e9949b4c313db454c88d0a45f35
reiikennedy <mutuakennedy81@gmail.com>
2023-02-18 23:28:08 +0300

    refactor: move image imports to global js file. Makes it easier to manage images in one js file

--- react/src/global/Images.js
+++ react/src/global/Images.js
@@ -2,9 +2,29 @@
 // This will make it easier to manage the across the project 
 
 import calendarApp from "../assets/calendar-app.jpg";
+import getApp from '../assets/getApp.png';
+import trusPilot from '../assets/truspilot.png';
+import capterra from '../assets/capterra.png';
+import hr from '../assets/hr.png';
+import airbnb from '../assets/airbnb.png';
+import instagram from '../assets/instagram.png';
+import github from '../assets/github.png';
+import snapchat from '../assets/snapchat.png';
+import logo from '../assets/solid.png';
+import dashboard from '../assets/dashboard.png';
 
 const Images = {
     calendarApp,
+    getApp,
+    trusPilot,
+    capterra,
+    hr,
+    airbnb,
+    instagram,
+    github,
+    snapchat,
+    logo,
+    dashboard
 }
 
 export default Images;
